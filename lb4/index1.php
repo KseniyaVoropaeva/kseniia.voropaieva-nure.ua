b12f71b60f5b95c45ad1b77a1e509f4a82a768d0
KseniyaVoropaeva <63408720+KseniyaVoropaeva@users.noreply.github.com>
2020-12-16 11:24:18 +0200

    Revert "lb"
    This reverts commit e66a3347443eb48f1d4efe8a0f8ffc0c52f1e056.

--- lb4/index1.php
+++
@@ -1,9 +0,0 @@
-<?php 
- if (!empty($_POST)) {
-  setcookie('name', $_POST['name']);
-  header("Location:index1.php");
- } else {
-  echo "Hello, ".$_COOKIE['name'];
-  echo ", welcome to our website";
- }
-?>
